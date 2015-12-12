1ff6122edf55d9d2b43a47be21e06e15f1882c51
Honza Soukup <jendasou@gmail.com>
2015-12-12 23:29:15 +0100

    Area,services section, buttons, basic construction added.

--- www/js/functions.js
+++ www/js/functions.js
@@ -3,3 +3,11 @@ $( document ).ready(function() {
   // Get started!
 
 });
+
+function Add(item,number){
+  var x=parseInt(document.getElementById(item).innerHTML);
+  var sum=x+number;
+  if(sum>0&&sum<210){
+    document.getElementById(item).innerHTML=x+number;
+  }
+}
